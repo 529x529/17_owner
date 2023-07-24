87604f67885827d7eb577f44c0a4164c5994514d
mikhailrezchikov <mrezchikov529@gmail.com>
2023-07-24 21:20:28 +0300

    hw 17 added

---
+++ src/test/java/com/github/tests/GithubWebTests.java
@@ -0,0 +1,20 @@
+package com.github.tests;
+import com.github.config.WebConfig;
+import org.aeonbits.owner.ConfigFactory;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class GithubWebTests extends TestBase {
+
+    @Test
+    void checkIssueNameTest() {
+
+        open("https://github.com");
+        $(".header-menu-wrapper").$(byText("Solutions")).click();
+        $(".border-bottom").$(byText("Enterprise")).click();
+        $(".eyebrow-banner").sibling(1).shouldHave(text("Build like the best"));
+    }
+}
---
+++ src/test/java/com/github/tests/TestBase.java
@@ -0,0 +1,32 @@
+package com.github.tests;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import com.github.config.WebConfig;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.aeonbits.owner.ConfigFactory;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+
+public class TestBase {
+
+    static WebConfig config = ConfigFactory.create(WebConfig.class, System.getProperties());
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1920x1080";
+        Configuration.pageLoadStrategy = "eager";
+        Configuration.baseUrl = config.baseUrl();
+        Configuration.browser = config.browser();
+        Configuration.browserVersion = config.browserVersion();
+        Configuration.browserSize = config.browserSize();
+        if (config.isRemote()) {
+            Configuration.remote = config.remoteUrl();
+        }
+    }
+
+    @BeforeEach
+    void addListener() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+    }
+}
